fc9720f69aa009139180ebcbe2b33c11567c87ea
Mxrcos13 <marcospantoja21@outlook.com>
2023-06-28 01:30:16 -0700

    rough version of the calculator does single digit arithmetic

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let displayValue;
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
 const add =  (a,b) => a + b;
 
 const subtract = (a,b) => a - b;
@@ -6,24 +11,84 @@ const multiply = (a,b) => a * b;
 
 const divide = (a,b) => a / b;
 
-const operate = (a,b,func) => func(a,b);
+const operate = () => operator(firstNum,secondNum);
 
 const getnum = (doc) => doc.textContent
 
-let displayValue;
-let firstNum;
-let secondNum;
-let operator;
+const numButtons = document.querySelectorAll('.buttons');
+const operatorButtons = document.querySelectorAll('.operator')
 
-const buttons = document.querySelectorAll('.buttons');
 const screen = document.querySelector('.screen');
 
-const handleClick = current => {
-    displayValue = current.target.value;
+const handleNumClick = current => {
+    displayValue = parseInt(current.target.value);
     screen.textContent = `${displayValue}`;
-    console.log(displayValue);
+    if(firstNum === 0){
+        firstNum = displayValue;
+    }else{
+        secondNum = displayValue;
+    }
 };
 
-buttons.forEach((button) =>{
-    button.addEventListener('click',handleClick)
+const handleOperatorClick = current =>{
+    displayValue = current.target.value;
+    switch(displayValue){
+        case '+' :
+            if(operator === ''){
+                operator = add;
+            }else{
+                displayValue = operate();
+                firstNum = displayValue;
+                operator = add;
+            }
+            break;
+        case '-' :
+            if(operator === ''){
+                operator = subtract;
+            }else{
+                displayValue = operate();
+                firstNum = displayValue;
+                operator = subtract;
+            }
+            break;
+        case '/' :
+            if(operator === ''){
+                operator = divide;
+            }else{
+                displayValue = operate();
+                firstNum = displayValue;
+                operator = divide;
+            }
+            break;
+        case '*' : 
+            if(operator === ''){
+                operator = multiply;
+            }else{
+                displayValue = operate();
+                firstNum = displayValue;
+                operator = multiply
+            }
+            break;
+        case '=' :
+             displayValue = operate();
+             firstNum = displayValue;
+             secondNum = 0;
+             operator = '';
+             break;
+        case 'CA' :
+            firstNum = 0;
+            secondNum = 0;
+            operator = '';
+            displayValue = '';
+            break;
+    }
+    screen.textContent = `${displayValue}`;
+}
+
+numButtons.forEach((button) =>{
+    button.addEventListener('click',handleNumClick);
 });
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', handleOperatorClick);
+})
+
