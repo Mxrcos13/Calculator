94f15f30949b97d51437c86df599792c87bc0995
Mxrcos13 <marcospantoja21@outlook.com>
2023-06-28 00:44:58 -0700

    added buttons that display when pressed on the calculator screen

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ const multiply = (a,b) => a * b;
 
 const divide = (a,b) => a / b;
 
+const operate = (a,b,func) => func(a,b);
+
+const getnum = (doc) => doc.textContent
+
+let displayValue;
 let firstNum;
 let secondNum;
 let operator;
 
-const operate = (a,b,func) => func(a,b);
+const buttons = document.querySelectorAll('.buttons');
+const screen = document.querySelector('.screen');
+
+const handleClick = current => {
+    displayValue = current.target.value;
+    screen.textContent = `${displayValue}`;
+    console.log(displayValue);
+};
+
+buttons.forEach((button) =>{
+    button.addEventListener('click',handleClick)
+});
